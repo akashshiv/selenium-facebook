682d37fbf50759372c5a6472b52afcbd1d5908ba
Akash Shivhare <akash@bldm2882-mac.corp.bluejeans.com>
2019-02-06 20:20:38 +0530

    add faltu file

---
+++ src/main/java/test_facebook/test_facebook/DownloadFileFromBrowser.java
@@ -0,0 +1,41 @@
+package test_facebook.test_facebook;
+
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Logger;
+
+import org.apache.log4j.PropertyConfigurator;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class DownloadFileFromBrowser {
+
+	public static void main(String[] args) {
+		
+//		System.setProperty("webdriver.chrome.driver", "/Users/akash/Downloads/chromedriver");
+//		
+//		ChromeOptions option = new ChromeOptions();
+//		option.setCapability("browser.helperApps.neverAsk.openFile", "application/octet-stream");
+//		
+//		WebDriver driver=new ChromeDriver(option);
+//		
+//		Logger log = Logger.getLogger("paytm");
+//		PropertyConfigurator.configure("log4j.properties");
+//		
+//		
+//		driver.manage().window().maximize();
+//		log.info("Window maximised");
+//		
+//		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+//		log.info("Perform Implicit wait");
+//		
+//		driver.get("http://www.filehippo.com/download_adobe_reader");
+//		log.info("open site");
+//		
+//		driver.findElement(By.xpath(".//*[@id='program-header']/div[4]/a[1]")).click();
+		
+	}
+
+}
